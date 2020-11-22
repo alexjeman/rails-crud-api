1aacaf2cbb883c120aa38c2dc3bda64099c6d898
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-23 01:39:12 +0200

    Add Todo controller and routes config

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,33 @@
 class TodosController < ApplicationController
+  # GET /todos
+  def index
+    @todos = Todo.all
+    render json: @todos, status: :ok
+  end
+
+  # GET /todos/:id
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo, status: :ok
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.create!(params.permit(:title, :created_by))
+    render json: @todo, status: :created
+  end
+
+  # PUT /todos/:id
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(params.permit(:title, :created_by))
+    head :no_content
+  end
+
+  # DELETE /todos/:id
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    head :no_content
+  end
 end
