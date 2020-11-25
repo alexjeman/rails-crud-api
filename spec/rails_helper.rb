8421c882fbd5118e712bc2fc398ad85e0f4c611e
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-25 22:04:04 +0200

    RSpec tests for Todo and Item models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -88,4 +88,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.default_formatter = 'doc'
 end
